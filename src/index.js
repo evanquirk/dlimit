b7716d1ad02285ebb422589bca9b975b6381fe3c
Evan <evan_quirk@me.com>
2022-08-25 12:28:04 -0700

    add normalize to index

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { render } from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
+import 'normalize.css';
 import App from './App'
 
 const rootElement = document.getElementById('root')
