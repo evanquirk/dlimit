c268fc56faab56a2149aa6d4a716d30c78c2fd02
Evan <evan_quirk@me.com>
2022-08-29 00:30:37 -0700

    add projectpage view file with content import

--- src/components/views/ProjectPage/index.jsx
+++ src/components/views/ProjectPage/index.jsx
@@ -1,7 +1,43 @@
-import React from 'react'
+//ProjectPage Index.js
 
-export const ProjectPage = () => {
+import React from 'react'
+import { useParams } from 'react-router-dom'
+import filterContent from './lib/filterContent'
+import { Pane, Text } from 'evergreen-ui'
+import { Banner, DoubleSpan, Film, Loader, SingleSpan, TitleCard, TripleSpan, Vertical, Video } from '../../base'
+import { Content, Info } from './components'
+
+
+export const ProjectPage = ({ work }) => {
+  const { slug, type } = useParams()
+
+  const project = work[type].find(obj => {
+    return obj.slug === slug
+  })
+
+  const { name, header, banner, roles, content} = project
+
   return (
-    <></>
+    <Pane
+      display='flex'
+      flexDirection='column'
+      justifyContent='center'
+      alignItems='center'
+    >
+      <Info
+        title={name}
+        roles={roles}
+        description={header}
+      />
+      {
+        content.map(({ sys, fields }, index) => {
+          const { contentType: { sys: { id: contentId } } } = sys
+          console.log(contentId)
+          // return filterContent({ id, fields, index })
+          return <Content key={`content-${index}`} contentId={contentId} fields={fields} />
+        })
+      }
+    </Pane>
+
   )
 }
