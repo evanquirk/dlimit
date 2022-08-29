ff490e49579f567d1aa25b6ddcfb386f3bff424b
Evan <evan_quirk@me.com>
2022-08-29 00:22:08 -0700

    add work page view

--- src/components/views/Work/index.jsx
+++ src/components/views/Work/index.jsx
@@ -1,7 +1,35 @@
-import React from 'react'
+// Work Page View
 
-export const Work = () => {
+import React, { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import { TitleCard } from '../../base'
+import { Pane } from 'evergreen-ui'
+
+export const Work = ({ work }) => {
+  const { type = 'all' } = useParams()
+
   return (
-    <></>
+    <Pane
+    display='flex'
+    flexWrap='wrap'
+    flexDirection='row'
+    justifyContent='center'
+    alignContent='center'
+    >
+      {work[type].map((project, index) => {
+        const { type, slug, card } = project
+        if (!type) {
+          return <></>
+        }
+        return (
+        <TitleCard
+          key={index}
+          type={type}
+          slug={slug}
+          fields={card.fields}
+        />
+      )})}
+    </Pane>
+
   )
 }
