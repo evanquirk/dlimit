4a8ee71891ad02b94ca40f45d017ed0b699a4aab
Evan <evan_quirk@me.com>
2022-08-28 23:49:32 -0700

    base component export index

---
+++ src/components/base/index.js
@@ -0,0 +1,10 @@
+// base components export
+
+export { Banner } from './Banner'
+export { DoubleSpan } from './DoubleSpan'
+export { Film } from './Film'
+export { Loader } from './Loader'
+export { TitleCard } from './TitleCard'
+export { TripleSpan } from './TripleSpan'
+export { Vertical } from './Vertical'
+export { Video } from './Video'
