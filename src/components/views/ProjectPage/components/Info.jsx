51abfd480d58d4be262ae78b2cc8b92363fdffbe
Evan <evan_quirk@me.com>
2022-09-02 22:57:10 -0700

    add styling to info componentfor project page, fix import

--- src/components/views/ProjectPage/components/Info.jsx
+++ src/components/views/ProjectPage/components/Info.jsx
@@ -2,7 +2,7 @@
 
 import React from 'react'
 import { Composition } from 'atomic-layout'
-import { Heading, Paragraph } from 'evergreen-ui'
+import { Heading, Paragraph, Text } from 'evergreen-ui'
 
 const area = `
   title description
@@ -10,18 +10,31 @@ const area = `
 `
 
 export const Info = ({ title, roles, description }) => {
+
   return (
     <Composition
       areas={area}
-      templateCols='1fr 3fr'
-      marginHorizontal='20'
+      templateCols='1fr 1fr'
+
+      marginHorizontal='5%'
     >
       {({ Title, Roles, Description }) => (
         <>
         <Title>
-          <Heading>{title}</Heading>
+          <Heading>{title.toUpperCase()}</Heading>
         </Title>
         <Roles>
+          {roles.map(({fields}) => {
+              const { preface, content } = fields
+              return (
+                <>
+                <Text><b>{preface}</b> </Text>
+                <Text>{content}</Text>
+                <br/>
+                </>
+              )
+          })
+          }
         </Roles>
         <Description>
           <Paragraph>{description}</Paragraph>
