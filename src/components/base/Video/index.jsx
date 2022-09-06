cdb4e3a4b0be62a577a8b55cb6ac79238952796c
Evan <evan_quirk@me.com>
2022-09-06 15:40:24 -0700

    add react-video to project, create video component

--- src/components/base/Video/index.jsx
+++ src/components/base/Video/index.jsx
@@ -1,7 +1,16 @@
+// Video Component
+// TODO - Add React-Player. Create Vertical or Full rendering.
+
 import React from 'react'
+import ReactPlayer from 'react-player'
 
 export const Video = (fields) => {
+
   return (
-    <div>Video</div>
+    <ReactPlayer
+      url='https://youtu.be/7sDY4m8KNLc'
+      width='75.8%'
+      height='900px'
+    />
   )
 }
