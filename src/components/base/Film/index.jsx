840e706eaa12e482d27c6cf95938a0aecd93225e
Evan <evan_quirk@me.com>
2022-09-05 20:25:25 -0700

    film component styling

--- src/components/base/Film/index.jsx
+++ src/components/base/Film/index.jsx
@@ -1,27 +1,29 @@
 // Film Component
-// TODO - Make Dialogue for full screen image pop-up. Provide CSS to wrap images at 4 x n 
+// TODO - Make Dialogue for full screen image pop-up.
 
 import React from 'react'
-import { Pane } from 'evergreen-ui'
+import { Pane, RepeatIcon } from 'evergreen-ui'
 
 export const Film = ({fields}) => {
   const photos = fields.photos
 
   return (
     <Pane
-      display='flex'
-      flex-flow='wrap'
+      display='grid'
+      gridTemplateColumns='repeat(4, 1fr)'
+      gap='10px'
       id='film'
     >
       { 
         photos.map((photo, index) => {
-          const { fields: { title, description, file: { url } } } = photo;
+          const { fields: { title, description, file: { url } } } = photo
             
           return (
               <img
                 src={`https:${url}`}
                 alt={description}
                 id={`f${index}`}
+                // onClick={}
               />
             )
         }) 
